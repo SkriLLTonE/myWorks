9db971bbcfa4358a83710c8daaeb15463355960d
SkriLLTonE <timurkazah1998@gmail.com>
2021-12-04 19:56:06 +0300

    FAQ
    FAQ added and adapted functionality

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,6 +7,23 @@ headcrab.addEventListener(("click"), () => {
     menu.classList.toggle("see");
 })
 
+let acc = document.getElementsByClassName("faq_accardion");
+let sym = document.getElementsByClassName("faq_symbol")
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+
+
 
 
 
